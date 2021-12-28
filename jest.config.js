c35f5400573b6841dc753c667811841fbe34ac1a
ygao <764714864@qq.com>
2021-12-28 15:50:21 -0500

    Use @testing-library/jest-dom to test the contents inside component
    1. @testing-library/react only test the rendering of the component. In order to test the content of component, we need other package call @testing-library/jest-dom
    2. Configuring Jest property: setupFilesAfterEnv to import the library @testing-library/jest-dom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
   globals: {
-    'ts-jest': {
+    "ts-jest": {
       // ts-jest configuration goes here
-      tsconfig: "tsconfig.jest.json"
+      tsconfig: "tsconfig.jest.json",
     },
   },
+  setupFilesAfterEnv: ["./jest.setup.ts"],
 };
--- src/test/Hello.test.tsx
+++ src/test/Hello.test.tsx
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Hello } from "../components/Hello";
 
 test("Check the rendering of component", () => {
   render(<Hello />);
+  const myComponent = screen.getByText(/Hello/);
+  expect(myComponent).toBeInTheDocument();
 });
