2ee0c1cf7264e4c5f7e0e761068275bf6955481f
ygao <764714864@qq.com>
2021-12-28 16:13:48 -0500

    Add more rules to eslint
    1. Run: npm install --save-dev eslint-plugin-jest that contains eslint rules for write better jest test code, then add those rules to eslint by change .eslintrc.js, and add "plugin:jest/recommended", "plugin:jest/style", to extends array.
    2. Run npm install --save-dev eslint-plugin-testing-library that contains more eslint rules for write better react test code, then add those rules to eslint by change .eslintrc.js, and add "plugin:testing-library/react",to extends array.
    3. We can easily add more rules by check out documents of those eslint-plugin

--- src/test/Hello.test.tsx
+++ src/test/Hello.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Hello } from "../components/Hello";
 
+// check eslint rule installed or not
+// fit("Check the rendering of component", () => {
+//   render(<Hello />);
+//   const myComponent = screen.getByText(/Hello/);
+//   expect(myComponent).toBeInTheDocument();
+// });
+
 test("Check the rendering of component", () => {
   render(<Hello />);
   const myComponent = screen.getByText(/Hello/);
+  // screen.debug();
   expect(myComponent).toBeInTheDocument();
 });
